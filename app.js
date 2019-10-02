b63c01a820084dfe7ecb3f2aa417febca8afe751
AaronMullan <aaroncmullan@gmail.com>
2019-10-01 18:14:54 -0700

    some design, working from front and back

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function compareNumbers = if (guess > correctNumber){
+        const comparison = 1
+}   else if (guess < correctNumber)  {
+        const comparison = -1
+}   else {
+        const comparison = 0
+}
--- test/number-guesser-test.js
+++ test/number-guesser-test.js
@@ -1,16 +1,18 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-
+//import isBlown from '../';
 const test = QUnit.test;
 
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    const correctNumber = 13;
     //Act 
     // Call the function you're testing and set the result to a const
 
     //Assert
     // Make assertions about what is expected valid result
-    assert.equal(true, false);
+    assert.equal(correctNumber, 13);
+    assert.equal(correctNumber < 13, false);
+    assert.equal(correctNumber > 13, false);
 });
